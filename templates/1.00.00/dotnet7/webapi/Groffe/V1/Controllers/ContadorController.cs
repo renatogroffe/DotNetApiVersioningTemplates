88ebedc885100f3404a4d01c00c90a94ea17e8af
Renato Groffe <renato.groffe@yahoo.com.br>
2023-08-02 12:16:52 -0300

    Inclusao Template Web Api

---
+++ templates/1.00.00/dotnet7/webapi/Groffe/V1/Controllers/ContadorController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Groffe.V1.Models;
+
+namespace Groffe.V1.Controllers;
+
+[ApiController]
+[ApiVersion("1.0", Deprecated = true)]
+[ApiVersion("1.1")]
+[ApiVersion("1.2")]
+[Route("[controller]")]
+[Route("v{version:apiVersion}/[controller]")]
+public class ContadorController : ControllerBase
+{
+    private static readonly Contador _CONTADOR = new Contador();
+    private readonly ILogger<ContadorController> _logger;
+    private readonly IConfiguration _configuration;
+
+    public ContadorController(ILogger<ContadorController> logger,
+        IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
+
+    [HttpGet, MapToApiVersion("1.1")]
+    [HttpGet, MapToApiVersion("1.2")]
+    public ResultadoContador GetV1_1()
+    {
+        int valorAtualContador;
+        lock (_CONTADOR)
+        {
+            _CONTADOR.Incrementar();
+            valorAtualContador = _CONTADOR.ValorAtual;
+        }
+
+        return GetResultadoContador(valorAtualContador);
+    }
+
+    [HttpGet, MapToApiVersion("1.0")]
+    public ResultadoContador GetV1_0()
+    {
+        int valorAtualContador;
+        lock (_CONTADOR)
+        {
+            _CONTADOR.Incrementar();
+            valorAtualContador = _CONTADOR.ValorAtual;
+        }
+
+        if (valorAtualContador % 4 == 0)
+        {
+            _logger.LogError("Simulando falha...");
+            throw new Exception("Simulação de falha!");
+        }
+
+        return GetResultadoContador(valorAtualContador);
+    }
+
+    private ResultadoContador GetResultadoContador(int valorAtualContador)
+    {
+        _logger.LogInformation($"Contador - Valor atual: {valorAtualContador}");
+
+        return new()
+        {
+            ValorAtual = valorAtualContador,
+            Local = _CONTADOR.Local,
+            Kernel = _CONTADOR.Kernel,
+            Framework = _CONTADOR.Framework,
+            Mensagem = _configuration["MensagemVariavel"]
+        };
+    }
+}
